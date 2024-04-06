4ad4840b28732c80023e446601ec522eaa2feda5
Marco Console <consolemarco1@gmail.com>
2024-04-06 20:07:51 +0200

    Esercitazioni terminate

---
+++ lw_04/esercitazioni/main.js
@@ -0,0 +1,67 @@
+// Prima esercitazione //
+
+//const MyBirth = '08/03/1992';
+//let ToDate = '08/03/1992';
+//if (MyBirth === ToDate) {
+//   console.log ("Buon compleanno!, ecco il tuo regalo 🎁");
+//}   else {
+        //console.log("Non è ancora il tuo compleanno");
+    //}
+
+// Seconda esercitazione //
+
+const MyRoutine = [
+    "Mi alzo dal letto", 
+    "Faccio colazione", 
+    "Mi lavo", 
+    "Mi vesto", 
+    "Preparo lo zaino", 
+    "Esco di casa", 
+    "Guido verso l'azienda", 
+    "Timbro il cartellino", 
+    "Lavoro sodo per 8 ore", 
+    "Torno a casa", 
+    "Studio le slide", 
+    "Seguo la lazione CB9", 
+    "Mangio la cena", 
+    "Eseguo questa esercitazione", 
+    "Vado a nanna"
+];
+for (let i=0; i < MyRoutine.lenght; i++) {
+    console.log(MyRoutine[i]);
+}
+
+// Esercitazione avanzata 1 //
+
+const MyBirth = prompt ("Inserisci la tua data di nascita");
+let ToDate = '08/03/1992';
+if (MyBirth === ToDate) {
+    console.log ("Buon compleanno!, ecco il tuo regalo 🎁");
+}   else {
+        console.log("Non è ancora il tuo compleanno");
+    }
+
+//Esercitazione avanzata 2 //
+
+function calcolatrice();{
+let FirstNum = prompt ("Inserisci il primo numero");
+let Operation = prompt ("Inserisci una delle seguenti operazioni (+, -, *, /):");
+let SecNum = prompt ("Inserisci il secondo numero");
+let Result;
+
+switch (Operation){
+    case '+':
+        Result = FirstNum + SecNum;
+    break;
+    case '-':
+        Result = FirstNum - SecNum;
+    break;
+    case '*':
+        Result = FirstNum * SecNum;
+    break;
+    case '/':
+        Result = FirstNum / SecNum;
+    break;
+}
+    console.log(Result);
+}
